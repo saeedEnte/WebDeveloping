a234503496319ccfa97ca5d4a8668af8348fb8b1
saeedEnte <saeed-entezari@uni-weimar.de>
2018-04-29 15:35:02 +0200

    The folder of teh League project is commited

---
+++ League/main.js
@@ -0,0 +1,8 @@
+function mainLeague() {
+    var num_of_teams = prompt("Please enter the number of teams","13");
+    // var teams = num_of_teams[num_of_teams]
+
+    for (let i = 0; i < num_of_teams.length; i++) {
+        teams.push(new Object());
+    }
+}
